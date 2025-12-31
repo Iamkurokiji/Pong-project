0620f29adbae9be2b6d966b84e0c30aa5ee3e5a5
Iamkurokiji <grantlimsaputra@gmail.com>
2025-12-31 18:23:50 +0700

    Ball Mechanics
    Balls will now spawn at a random angle and random direction as it will also reset upon colliding with the scoring area

---
+++ Assets/Apps/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float maxInitialAngle = 0.67f;
+    public float movSpeed = 1f;
+    private float startX = 0f;
+    public float maxStartY = 4f;
+    // Start is called before the first fame update
+    void Start()
+    {
+        Vector2 direc = Random.value < 0.5f ? Vector2.left : Vector2.right;
+        direc.y = Random.Range(-maxInitialAngle, maxInitialAngle);
+        rb2d.velocity = direc * movSpeed;
+    }
+
+    private void ResetBall()
+    {
+        float posY = Random.Range(-maxStartY, maxStartY);
+        Vector2 position = new Vector2(startX, posY);
+        transform.position = position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ScoreZoneScript scoreZone = collision.GetComponent<ScoreZoneScript>(); //Scriptname variable = collision.GetComponent<scriptname>();
+        if(scoreZone)
+        {
+             UnityEngine.Debug.Log("Scored!");
+             ResetBall();
+             Start();
+        }
+    }
+}
